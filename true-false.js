781ebf6a27433c67a069b914a2c95e9fa34422ca
khalid hasan ifrad <khalidhasanifrad1@gmail.com>
2021-02-11 19:11:00 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,11 @@
+// const name = "ifrad";
+let name = 0;
+if (name || name == 0) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// falsy value: false, "", 0, undefined, null, NaN
+// truthy value: "0", " ", {}, []
