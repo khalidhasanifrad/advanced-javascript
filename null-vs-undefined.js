e8a0f1e49ad561cff95249502f2095977ffec25c
khalid hasan ifrad <khalidhasanifrad1@gmail.com>
2021-02-11 20:14:57 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+let ifrad;
+console.log(ifrad);
+
+function add(num1, num2){
+    const added = (num1 + num2);
+    return ;
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+
+}
+const result = add(20);
+console.log(result);
+const details = {name: "ifrad", phone: 01735353215, job: "web-developer"};
+console.log(details.salary);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [20, 21, 22, 23];
+console.log(ages[7]);
+
